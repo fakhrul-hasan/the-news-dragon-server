ec96285a7a1428ea58be9e7202bc42bb106a25c1
fakhrul-hasan <mdfakhrulh7@gmail.com>
2023-04-21 01:39:21 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 5000
+const categories = require('./data/categories.json')
+const cors = require('cors')
+
+app.use(cors())
+app.get('/',(req, res)=>{
+    res.send('Dragon is running');
+})
+app.get('/categories',(req,res)=>{
+    res.send(categories);
+})
+app.listen(port,()=>{
+    console.log(`Dragon is running on port ${port}`);
+})
